18ff1e97e77413414deb37f1ca341d4976a21553
Miz <mizzim46@gmail.com>
2015-07-23 13:18:28 +0200

    Modification du type email

--- query/login.php
+++ query/login.php
@@ -2,7 +2,7 @@
 if (isset($data['login'])) {
 	$Login = "SELECT * 
 			FROM utilisateurs
-			WHERE email = '".htmlspecialchars($data['login'])."'
+			WHERE login = '".htmlspecialchars($data['login'])."'
 			AND password = '".crypt(htmlspecialchars($data['password']), '$2y$08$'.__SALTCRYPT__.'$')."'";
 
     $LoginReponse = $app['db']->fetchAssoc($Login);

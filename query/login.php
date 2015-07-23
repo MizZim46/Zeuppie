fb7371e1a3e340003ca9dd3d3ef79b78945426d7
Miz <mizzim46@gmail.com>
2015-07-23 13:15:49 +0200

    Modification du type de chiffrement

--- query/login.php
+++ query/login.php
@@ -2,8 +2,8 @@
 if (isset($data['login'])) {
 	$Login = "SELECT * 
 			FROM utilisateurs
-			WHERE email = '".$data['login']."'
-			AND password = '".md5($data['password'])."'";
+			WHERE email = '".htmlspecialchars($data['login'])."'
+			AND password = '".crypt(htmlspecialchars($data['password']), '$2y$08$'.__SALTCRYPT__.'$')."'";
 
     $LoginReponse = $app['db']->fetchAssoc($Login);
 }

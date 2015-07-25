fb8156b4293bf82e2540bcb64932da76ca066aa3
Miz <mizzim46@gmail.com>
2015-07-25 14:44:34 +0200

    Ajout des catégories menu

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -5,12 +5,13 @@ session_start();
 
 include '../query/utilisateurs.php';
 include '../query/articles.php';
+include '../query/categories.php';
 
 if (isset($_SESSION['login'])) {
 
 $layout = $app['twig']->loadTemplate('layout.twig');
 
-    return $app['twig']->render('index.twig', array('users' => $_SESSION['login'], 'infousers' => $utilisateursReponse, 'nbnotifs' => $NbNotificationsReponse['nbNotifs'], 'notifications' => $NotificationsReponse, 'articles' => $ArticlesReponse, 'layout' => $layout));
+    return $app['twig']->render('index.twig', array('users' => $_SESSION['login'], 'infousers' => $utilisateursReponse, 'nbnotifs' => $NbNotificationsReponse['nbNotifs'], 'notifications' => $NotificationsReponse, 'articles' => $ArticlesReponse, 'categories' => $CategoriesReponse, 'layout' => $layout));
 }
 else {
 	return $app->redirect('inscription');
